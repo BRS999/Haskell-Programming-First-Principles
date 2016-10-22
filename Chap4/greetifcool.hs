1f5e3ba870519f024878daf7d2ef0ee5ba1331dc
Ben <BRS999@users.noreply.github.com>
2016-10-22 16:43:17 -0400

    Delete greetifcool.hs

--- Chap4/greetifcool.hs
+++
@@ -1,9 +0,0 @@
-module GreetIfCool where
-
-    greetIfCool :: String -> IO()
-    greetIfCool coolness =
-        if cool coolness
-            then putStrLn "shakin"
-        else
-            putStrLn "pssshhh"
-        where cool v = v == "frosty"
