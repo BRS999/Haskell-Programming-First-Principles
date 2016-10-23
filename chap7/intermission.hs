f7012bc1e3b023a12727b805a6be99d033c56f05
Ben <ben.richard.spencer@me.com>
2016-10-23 10:26:38 -0400

    chap7

--- chap7/intermission.hs
+++ chap7/intermission.hs
@@ -13,3 +13,40 @@ module Intermission where
 
     f :: (a, b, c) -> (d, e, f) -> ((a, d), (c, f)) 
     f (a, b, c) (d, e, f) = ((a, d), (c , f))
+
+    --Case expression
+    --functionC x y = if (x > y) then x else y
+    functionC x y = 
+        case x > y of 
+            True -> x
+            False -> y
+
+    -- ifEvenAdd2 n = if even n then (n+2) else n
+    ifEvenAdd2 n =
+        case n `mod` 2 == 0 of
+            True -> n + 2
+            False -> n
+
+    nums x =
+        case compare x 0 of
+            LT -> -1 
+            GT -> 1
+            EQ -> 0
+
+    -- Dodgy
+    
+    dodgy x y = x + y * 10 
+    oneIsOne = dodgy 1 
+    oneIsTwo = (flip dodgy) 2
+
+    -- dodgy 1 0 = 1
+    -- dodgy 1 1 = 11
+    -- dodgy 2 2 = 22
+    -- dodgy 1 2 = 21
+    -- dodgy 2 1 = 12
+    -- oneIsOne 1 = 11
+    -- oneIsOne 2 = 21
+    -- oneIsTwo 1 = 21
+    -- oneIsTwo 2 = 22
+    -- oneIsOne 3 = 31
+    -- oneIsTwo 3 = 23 
--- chap7/notes.md
+++ chap7/notes.md
@@ -17,3 +17,10 @@ Resolving a value depends on the location in the code
 
 newtype is a special case of data.
 It only permits one constructor and one field.
+
+### Higher Order Functions
+
+Are functions that accept other functions as arguments
+Since functions are just values they can be passed around as arguments!
+
+
