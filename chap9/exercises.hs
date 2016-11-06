b5faffa6cc1643c1b1f49680db8acc00ac82ca1d
Ben <ben.richard.spencer@me.com>
2016-11-05 20:59:44 -0400

    tyger

--- chap9/exercises.hs
+++ chap9/exercises.hs
@@ -13,3 +13,35 @@ module Exercises where
 
     eftChar :: Char -> Char -> [Char]
     eftChar x y = enumFromTo x y :: [Char]
+
+    -- Exercises: Thy Fearful Symmetry
+    fun = "all i wanna do is have some fun"
+
+    myWords :: String -> [String]
+    myWords x
+        | dropWhile (/=' ') x == "" = [x]
+        | otherwise = (takeWhile (/=' ') x) : myWords(dropWhile (==' ') (dropWhile (/=' ') x))
+
+    firstSen = "Tyger Tyger, burning bright\n" 
+    secondSen = "In the forests of the night\n" 
+    thirdSen = "What immortal hand or eye\n" 
+    fourthSen = "Could frame thy fearful symmetry?" 
+    sentences = firstSen ++ secondSen ++ thirdSen ++ fourthSen
+  
+  
+    myLines :: String -> [String] 
+    myLines x
+        | dropWhile (/='\n') x == "" = [x]
+        | otherwise = (takeWhile (/='\n') x) : myLines(dropWhile (=='\n') (dropWhile (/='\n') x))
+
+    shouldEqual =
+        [ "Tyger Tyger, burning bright"
+        , "In the forests of the night"
+        , "What immortal hand or eye"
+        , "Could frame thy fearful symmetry?" ] 
+
+
+    main :: IO ()
+    main =
+        print $ "Are they equal? " 
+            ++ show (myLines sentences == shouldEqual)
