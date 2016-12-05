75546c56e6d3fcda0a312872cced7e3bd47b99f1
bspencer <bspencer@conductor.com>
2016-12-04 20:11:11 -0500

    maybeee

--- chap12/exercises.hs
+++ chap12/exercises.hs
@@ -34,3 +34,46 @@ module Exercises where
     mkWord x 
        | countConsonants x > countVowels x = Just $ Word' x
        | otherwise = Nothing
+
+    isJust :: Maybe a -> Bool
+    isJust Nothing = False
+    isJust (Just _) = True
+
+    isNothing :: Maybe a -> Bool
+    isNothing = not . isJust
+
+    mayybee :: b -> (a -> b) -> Maybe a -> b
+    mayybee g f x =
+        case x of
+            Nothing -> g
+            Just x -> f x
+
+    listToMaybe :: [a] -> Maybe a
+    listToMaybe [] = Nothing
+    listToMaybe (x : _) = Just x
+
+    maybeToList :: Maybe a -> [a]
+    maybeToList Nothing = []
+    maybeToList (Just x) = [x]
+
+    -- Iteration 
+
+    myIterate :: (a -> a) -> a -> [a]
+    myIterate f a = a : (myIterate f (f a))
+    
+    myUnfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+    myUnfoldr f x = 
+        case (f x) of 
+            Just (a , b) -> a : myUnfoldr f b
+            Nothing -> []
+
+    data BinaryTree a = 
+        Leaf
+      | Node (BinaryTree a) a (BinaryTree a) 
+      deriving (Eq, Ord, Show)
+
+    unfold :: (a -> Maybe (a,b,a)) -> a -> BinaryTree b
+    unfold f x =
+        case (f x) of 
+            Nothing -> Leaf
+            Just (a, b, c) -> Node (unfold f a) b (unfold f c)
